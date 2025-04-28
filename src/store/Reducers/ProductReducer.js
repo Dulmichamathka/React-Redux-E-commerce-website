612a603de9db63d0b61f751e1197a8648289b8ec
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2025-04-28 15:56:45 +0530

    create actions,reducers and completely structured the project for using redux

---
+++ src/store/Reducers/ProductReducer.js
@@ -0,0 +1,21 @@
+import { ActionType } from "../Constants/action-types";
+
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Dulmi",
+      category: "Programmer",
+    },
+  ],
+};
+
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionType.SET_PRODUCTS:
+      return state;
+
+    default:
+      return state;
+  }
+};
